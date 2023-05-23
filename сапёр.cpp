9d2dae4ac8547e5b2dfb040c97e9f95f5a030e07
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2023-05-23 11:27:00 +0300

    Update сапёр.cpp

--- сапёр.cpp
+++ сапёр.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 HANDLE hand = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD cord;
 
 void FieldCreation(vector<vector<char>>& field, vector<vector<char>>& mask)
 {
@@ -17,62 +16,30 @@ void FieldCreation(vector<vector<char>>& field, vector<vector<char>>& mask)
 
         for (int j = 0; j < 11; j++)
         {
-            if (i == 0)
+            if (i > 0 && j > 0 && i < 10 && j < 10)
             {
-                if (j == 0 || j == 10)
-                {
-                    FieldLine.push_back(' ');
-
-                    MaskLine.push_back(' ');
-                }
-                else
-                {
-                    FieldLine.push_back(j + '0');
-
-                    MaskLine.push_back(' ');
-                }
-            }
-            else if (j == 0)
-            {
-                if (i == 10)
-                {
-                    FieldLine.push_back(' ');
-
-                    MaskLine.push_back(' ');
-                }
-                else
-                {
-                    FieldLine.push_back(i + '0');
-
-                    MaskLine.push_back(' ');
-                }
+                MaskLine.push_back('#');
             }
             else
             {
-                if (!(rand() % 8))
-                {
-                    if (i < 10 && j < 10)
-                    {
-                        FieldLine.push_back('*');
-                    }
-                    else
-                    {
-                        FieldLine.push_back(' ');
-                    }
-                }
-                else
-                {
-                    FieldLine.push_back(' ');
-                }
+                MaskLine.push_back(' ');
+            }
 
-                if (i < 10 && j < 10)
-                {
-                    MaskLine.push_back('#');
-                }
-                else
-                {
-                    MaskLine.push_back(' ');
-                }
+            if ((i == 0 && j == 0) || i > 9 || j > 9)
+            {
+                FieldLine.push_back(' ');
+            }
+            else if (i == 0 && j < 10)
+            {
+                FieldLine.push_back(j + '0');
+            }
+            else if (j == 0 && i < 10)
+            {
+                FieldLine.push_back(i + '0');
+            }
+            else if (i > 0 && j > 0 && i < 10 && j < 10)
+            {
+                FieldLine.push_back('0');
             }
         }
 
@@ -80,11 +47,16 @@ void FieldCreation(vector<vector<char>>& field, vector<vector<char>>& mask)
         mask.push_back(MaskLine);
     }
 
+    return;
+}
+
+void BombsNumber(vector<vector<char>>& field)
+{
     for (int i = 1; i < 10; i++)
     {
         for (int j = 1; j < 10; j++)
         {
-            if (field[i][j] == ' ')
+            if (field[i][j] == '0')
             {
                 int count = 0;
 
@@ -109,10 +81,6 @@ void FieldCreation(vector<vector<char>>& field, vector<vector<char>>& mask)
 
 void FieldOutput(vector<vector<char>>& field, vector<vector<char>>& mask)
 {
-    /*cord.X = 0;
-    cord.Y = 0;
-    SetConsoleCursorPosition(hand, cord);*/
-
     system("cls");
 
     for (int i = 0; i < 11; i++)
@@ -136,6 +104,12 @@ void FieldOutput(vector<vector<char>>& field, vector<vector<char>>& mask)
 
                 cout << field[i][j] << " ";
             }
+            else if (mask[i][j] == char(30))
+            {
+                SetConsoleTextAttribute(hand, 12);
+
+                cout << mask[i][j] << " ";
+            }
             else
             {
                 SetConsoleTextAttribute(hand, 15);
@@ -150,7 +124,8 @@ void FieldOutput(vector<vector<char>>& field, vector<vector<char>>& mask)
 
 bool Check(vector<vector<char>>& field, vector<vector<char>>& mask)
 {
-    int MasksNumber = 0, BombsNumber = 0;
+    int BombsNumber = 0, FlagsNumber = 0;
+    bool victory = true;
 
     for (int i = 1; i < 10; i++)
     {
@@ -161,14 +136,98 @@ bool Check(vector<vector<char>>& field, vector<vector<char>>& mask)
                 BombsNumber++;
             }
 
-            if (mask[i][j] == '#')
+            if (mask[i][j] == char(30))
             {
-                MasksNumber++;
+                FlagsNumber++;
+            }
+
+            if (field[i][j] == '*' && mask[i][j] != char(30))
+            {
+                victory = false;
             }
         }
     }
 
-    return MasksNumber == BombsNumber;
+    return victory && BombsNumber == FlagsNumber;
+}
+
+void OpenZeroPositions(vector<vector<char>>& field, vector<vector<char>>& mask, int x, int y)
+{
+    for (int i = x - 1; i < x + 2; i++)
+    {
+        for (int j = y - 1; j < y + 2; j++)
+        {
+            if (field[i][j] == '0' && mask[i][j] == '#')
+            {
+                mask[i][j] = ' ';
+
+                OpenZeroPositions(field, mask, i, j);
+            }
+        }
+    }
+
+    for (int i = 1; i < 10; i++)
+    {
+        for (int j = 1; j < 10; j++)
+        {
+            if (field[i][j] != '0')
+            {
+                for (int i0 = i - 1; i0 < i + 2; i0++)
+                {
+                    for (int j0 = j - 1; j0 < j + 2; j0++)
+                    {
+                        if (field[i0][j0] == '0' && mask[i0][j0] == ' ')
+                        {
+                            mask[i][j] = ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return;
+}
+
+void GameOver(vector<vector<char>>& field, vector<vector<char>>& mask)
+{
+    if (Check(field, mask))
+    {
+        system("cls");
+
+        FieldOutput(field, mask);
+
+        SetConsoleTextAttribute(hand, 10);
+
+        cout << "victory!" << endl;
+
+        SetConsoleTextAttribute(hand, 15);
+    }
+    else
+    {
+        system("cls");
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (field[i][j] == '*')
+                {
+                    mask[i][j] = ' ';
+                }
+            }
+        }
+
+        FieldOutput(field, mask);
+
+        SetConsoleTextAttribute(hand, 12);
+
+        cout << "game over!" << endl;
+
+        SetConsoleTextAttribute(hand, 15);
+    }
+
+    return;
 }
 
 int main()
@@ -177,63 +236,98 @@ int main()
 
     vector<vector<char>> field;
     vector<vector<char>> mask;
+    bool alive = true;
+    int bombs = 9;
 
     FieldCreation(field, mask);
 
     FieldOutput(field, mask);
 
-    while (true)
+    while (alive)
     {
         pair<int, int> pos;
+        int action;
 
-        cout << "open a position: ";
+        cout << "actions: " << endl;
+        cout << "1) open position" << endl;
+        cout << "2) put flag" << endl;
+        cout << "3) remove flag" << endl;
+        cout << endl;
+
+        cin >> action;
         cin >> pos.first;
         cin >> pos.second;
 
-        if (pos.first > 0 && pos.second > 0 && pos.first < 10 && pos.second < 10)
+        while (bombs > 0)
+        {
+            int tempX = 1 + rand() % 9, tempY = 1 + rand() % 9;
+
+            if (field[tempX][tempY] == '0' && tempX != pos.first && tempY != pos.second)
+            {
+                field[tempX][tempY] = '*';
+
+                bombs--;
+            }
+        }
+
+        if (bombs == 0)
+        {
+            BombsNumber(field);
+
+            bombs = -1;
+        }
+
+        if (action == 1 && mask[pos.first][pos.second] != char(30) && pos.first > 0 && pos.second > 0 && pos.first < 10 && pos.second < 10)
         {
             mask[pos.first][pos.second] = ' ';
 
+            if (field[pos.first][pos.second] == '0')
+            {
+                OpenZeroPositions(field, mask, pos.first, pos.second);
+            }
+
             FieldOutput(field, mask);
 
-            if (field[pos.first][pos.second] == '*')
+            if (field[pos.first][pos.second] == '*' || Check(field, mask))
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    for (int j = 0; j < 10; j++)
-                    {
-                        if (field[i][j] == '*')
-                        {
-                            mask[i][j] = ' ';
-                        }
-                    }
-                }
-
-                FieldOutput(field, mask);
-
-                Sleep(1000);
-
-                system("cls");
-
-                SetConsoleTextAttribute(hand, 12);
-
-                cout << "game over!" << endl;
-
-                break;
+                alive = false;
             }
+        }
+        else if (action == 2 && pos.first > 0 && pos.second > 0 && pos.first < 10 && pos.second < 10)
+        {
+            if (mask[pos.first][pos.second] == '#')
+            {
+                mask[pos.first][pos.second] = char(30);
+            }
+
+            FieldOutput(field, mask);
 
             if (Check(field, mask))
             {
-                Sleep(1000);
-
-                system("cls");
-
-                SetConsoleTextAttribute(hand, 10);
-
-                cout << "victory!" << endl;
-
-                break;
+                alive = false;
             }
         }
+        else if (action == 3 && pos.first > 0 && pos.second > 0 && pos.first < 10 && pos.second < 10)
+        {
+            if (mask[pos.first][pos.second] == char(30))
+            {
+                mask[pos.first][pos.second] = '#';
+            }
+
+            FieldOutput(field, mask);
+
+            if (Check(field, mask))
+            {
+                alive = false;
+            }
+        }
+        else
+        {
+            system("cls");
+
+            FieldOutput(field, mask);
+        }
     }
+
+    GameOver(field, mask);
 }
